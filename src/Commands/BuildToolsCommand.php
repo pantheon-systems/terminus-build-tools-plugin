9a17f6f58785de52040f9c7416a60f05eb6090b6
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-04-20 09:01:37 -0700

    Improve validation. Check site name before prompting for anything. Prompt for a team if one is not provided. (#30)

--- src/Commands/BuildToolsCommand.php
+++ src/Commands/BuildToolsCommand.php
@@ -105,6 +105,74 @@ class BuildToolsCommand extends TerminusCommand implements SiteAwareInterface
     }
 
     /**
+     * Return the list of available organizations
+     */
+    protected function availableOrgs()
+    {
+        $orgs = array_map(
+            function ($org) {
+                return $org->getLabel();
+            },
+            $this->session()->getUser()->getOrganizations()
+        );
+        return $orgs;
+    }
+
+    /**
+     * Validate requested site name before prompting for additional information.
+     *
+     * @hook init build-env:create-project
+     */
+    public function validateSiteName(InputInterface $input, AnnotationData $annotationData)
+    {
+        $github_org = $input->getOption('org');
+        $site_name = $input->getOption('pantheon-site');
+        $source = $input->getArgument('source');
+        $target = $input->getArgument('target');
+
+        // If only one parameter was provided, then it is the TARGET
+        if (empty($target)) {
+            $target = $source;
+            $source = 'd8';
+        }
+
+        // If the source site is a common alias, then replace it with its expanded value
+        $source = $this->expandSourceAliases($source);
+
+        // If an org was not provided for the source, then assume pantheon-systems
+        if (strpos($source, '/') === FALSE) {
+            $source = "pantheon-systems/$source";
+        }
+
+        // If an org was provided for the target, then extract it into
+        // the `$org` variable
+        if (strpos($target, '/') !== FALSE) {
+            list($github_org, $target) = explode('/', $target, 2);
+        }
+
+        // If the user did not explicitly provide a Pantheon site name,
+        // then use the target name for that purpose. This will probably
+        // be the most common usage -- with matching GitHub / Pantheon
+        // site names.
+        if (empty($site_name)) {
+            $site_name = $target;
+        }
+
+        // Before we begin, check to see if the requested site name is
+        // available on Pantheon, and fail if it is not.
+        $site_name = strtr(strtolower($site_name), '_ ', '--');
+        if ($this->sites()->nameIsTaken($site_name)) {
+            throw new TerminusException('The site name {site_name} is already taken on Pantheon.', compact('site_name'));
+        }
+
+        // Assign variables back to $input after filling in defaults.
+        $input->setArgument('source', $source);
+        $input->setArgument('target', $target);
+        $input->setOption('org', $github_org);
+        $input->setOption('pantheon-site', $site_name);
+    }
+
+    /**
      * Ensure that the user has provided credentials for GitHub and Circle CI,
      * and prompt for them if they have not.
      *
@@ -116,25 +184,54 @@ class BuildToolsCommand extends TerminusCommand implements SiteAwareInterface
     {
         // Ask for a GitHub token if one is not available.
         $github_token = getenv('GITHUB_TOKEN');
-        if (empty($github_token)) {
+        while (empty($github_token)) {
             $github_token = $this->io()->askHidden("Please visit the page https://github.com/settings/tokens to generate a GitHub personal access token token, as described in https://help.github.com/articles/creating-an-access-token-for-command-line-use. Give it the 'repo' and 'delete-repo' scopes.\nThen, enter it here:");
             $github_token = trim($github_token);
             putenv("GITHUB_TOKEN=$github_token");
+
+            // Validate that the GitHub token looks correct. If not, prompt again.
+            if ((count($github_token) < 40) || preg_match('#[^0-9a-fA-F]#', $github_token)) {
+                $this->log()->warning('GitHub tokens should be 40-character strings containing only the letters a-f and digits (0-9). Please enter your token again.');
+                $github_token = '';
+            }
         }
 
         // Ask for a Circle token if one is not available.
         $circle_token = getenv('CIRCLE_TOKEN');
-        if (empty($circle_token)) {
+        while (empty($circle_token)) {
             $circle_token = $this->io()->askHidden("Please visit the page https://circleci.com/account/api to generate a Circle CI personal API token, as described in https://circleci.com/docs/api/v1-reference/#getting-started\nThen, enter it here:");
             $circle_token = trim($circle_token);
             putenv("CIRCLE_TOKEN=$circle_token");
+
+            // Validate that the CircleCI token looks correct. If not, prompt again.
+            if ((count($circle_token) < 40) || preg_match('#[^0-9a-fA-F]#', $circle_token)) {
+                $this->log()->warning('GitHub tokens should be 40-character strings containing only the letters a-f and digits (0-9). Please enter your token again.');
+                $circle_token = '';
+            }
         }
 
         // If the user did not specify an admin password, then prompt for one.
         $adminPassword = $input->getOption('admin-password');
         if (empty($adminPassword)) {
+            $adminPassword = getenv('ADMIN_PASSWORD');
+        }
+        if (empty($adminPassword)) {
             $adminPassword = $this->io()->askHidden("Enter the password you would like to use to log in to your test site, or leave empty for a random password:");
-            $input->setOption('admin-password', $adminPassword);
+        }
+        $input->setOption('admin-password', $adminPassword);
+
+        // Encourage the user to select a team
+        $team = $input->getOption('team');
+        if (empty($team)) {
+            $team = getenv('TERMINUS_TEAM');
+        }
+        if (empty($team)) {
+            $orgs = array_values($this->availableOrgs());
+            array_unshift($orgs, '-');
+            $team = $this->io()->choice('Select a team for this site', $orgs);
+        }
+        if ($team != '-') {
+            $input->setOption('team', $team);
         }
     }
 
@@ -186,53 +283,14 @@ class BuildToolsCommand extends TerminusCommand implements SiteAwareInterface
 
         // Copy options into ordinary variables
         $github_org = $options['org'];
-        $team = $options['team'];
         $site_name = $options['pantheon-site'];
+        $team = $options['team'];
         $label = $options['label'];
         $stability = $options['stability'];
-
-        // If only one parameter was provided, then it is the TARGET
-        if (empty($target)) {
-            $target = $source;
-            $source = 'd8';
-        }
-
-        // If the source site is a common alias, then replace it with its expanded value
-        $source = $this->expandSourceAliases($source);
-
-        // If an org was not provided for the source, then assume pantheon-systems
-        if (strpos($source, '/') === FALSE) {
-            $source = "pantheon-systems/$source";
-        }
-
-        // If an org was provided for the target, then extract it into
-        // the `$org` variable
-        if (strpos($target, '/') !== FALSE) {
-            list($github_org, $target) = explode('/', $target, 2);
-        }
-
-        // If the user did not explicitly provide a Pantheon site name,
-        // then use the target name for that purpose. This will probably
-        // be the most common usage -- with matching GitHub / Pantheon
-        // site names.
-        if (empty($site_name)) {
-            $site_name = $target;
-        }
 
         // Provide default values for other optional variables.
         if (empty($label)) {
           $label = $site_name;
-        }
-
-        if (empty($team)) {
-            $team = getenv('TERMINUS_TEAM');
-        }
-
-        // Before we begin, check to see if the requested site name is
-        // available on Pantheon, and fail if it is not.
-        $site_name = strtr(strtolower($site_name), '_ ', '--');
-        if ($this->sites()->nameIsTaken($site_name)) {
-            throw new TerminusException('The site name {site_name} is already taken on Pantheon.', compact('site_name'));
         }
 
         // Get our authenticated credentials from environment variables.
