79c2f46f24cc22727e394118efdd63e3d481e427
Steve Persch <steve.persch@pantheon.io>
2017-07-11 18:29:09 -0500

    More bash fixes

--- .circleci/test-example-repo.sh
+++ .circleci/test-example-repo.sh
@@ -13,11 +13,11 @@ BUILD_TOOLS_VERSION=${PR_BRANCH:-$CIRCLE_BRANCH}
 
 
 
-- terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
+terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
 # Confirm that the Pantheon site was created
-- terminus site:info "$TERMINUS_SITE"
+terminus site:info "$TERMINUS_SITE"
 # Confirm that the Github project was created
-- git clone "https://github.com/${TARGET_REPO}.git" "$TARGET_REPO_WORKING_COPY"
+git clone "https://github.com/${TARGET_REPO}.git" "$TARGET_REPO_WORKING_COPY"
 # Confirm that Circle was configured for testing, and that the first test passed.
 # This process is slow and fragile, so we will not bother to re-run on the master branch.
 
