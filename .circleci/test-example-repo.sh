adf53dc95a091fa29cf0d99f3a55834ea572a72a
Steve Persch <steve.persch@pantheon.io>
2017-07-12 16:32:40 -0500

    Circle Watch

--- .circleci/test-example-repo.sh
+++ .circleci/test-example-repo.sh
@@ -6,11 +6,12 @@ set -ex
 
 TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
 
+BRANCH: $(echo $CIRCLE_BRANCH | grep -v '^\(master\|[0-9]\+.x\)$')
+PR_BRANCH: ${BRANCH:+dev-$BRANCH}
 SOURCE_COMPOSER_PROJECT=pantheon-systems/example-drops-8-composer:dev-test-2.x
 TARGET_REPO=$GITHUB_USERNAME/$TERMINUS_SITE
 TARGET_REPO_WORKING_COPY=$HOME/$TERMINUS_SITE
 BUILD_TOOLS_VERSION=${PR_BRANCH:-$CIRCLE_BRANCH}
-
 
 
 terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
@@ -22,12 +23,4 @@ git clone "https://github.com/${TARGET_REPO}.git" "$TARGET_REPO_WORKING_COPY"
 # This process is slow and fragile, so we will not bother to re-run on the master branch.
 
 cd "$TARGET_REPO_WORKING_COPY"
-
-
-
-
-
-# DON'T UNCOMMENT THIS WITHOUT ALTER THE LINE AT THE TOP OF THE FILE THAT
-# CAUSE ALL OUTPUT TO BE PRINTED. CONSIDER MOVING circle token OUTSIDE THIS FILE
-# circle token "$CIRCLE_TOKEN"
-# circle watch
+circle watch
