436e505ace127f48f2310c97793cb8894764549d
Steve Persch <steve.persch@pantheon.io>
2017-07-13 14:57:06 -0500

    comment update to retrigger build

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -23,5 +23,5 @@ touch $HOME/.ssh/config
 echo "StrictHostKeyChecking no" >> "$HOME/.ssh/config"
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "Circle CI"
-# Ignore file permissions
+# Ignore file permissions.
 git config --global core.fileMode false
