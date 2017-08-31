ff8f3cb69b9501e28586d595fe17c72a7fddb77f
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-08-30 18:25:40 -0700

    Add an ssh-key delete step to cleanup-fixtures script.

--- .circleci/cleanup-fixtures.sh
+++ .circleci/cleanup-fixtures.sh
@@ -4,4 +4,8 @@ set -e
 
 TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
 
+# Delete our github repository and Pantheon site
 terminus build:env:obliterate -n --yes "$TERMINUS_SITE"
+
+# Delete any ssh key we may have created for this test
+for key in $(terminus ssh-key:list --fields=Description,ID 2>/dev/null | grep "ci-bot-build-tools-$CIRCLE_BUILD_NUM" | sort -r | sed -e '1,3d' | sed -e 's/ *[^ ]* *//') ; do echo "Remove $key"; terminus ssh-key:remove $key ; done
