a2539e023aecbbd1d1d1c87f2a2dbf514c51f4e6
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-04-06 14:35:36 -0700

    Json content type for circle (#138)
    * Export environment variables, e.g. to hide terminus update messages.
    
    * Use json content type to talk to CircleCI.

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#=====================================================================================================================
+# EXPORT needed environment variables
+#
+# Circle CI 2.0 does not yet expand environment variables so they have to be manually EXPORTed
+# Once environment variables can be expanded this section can be removed
+# See: https://discuss.circleci.com/t/unclear-how-to-work-with-user-variables-circleci-provided-env-variables/12810/11
+# See: https://discuss.circleci.com/t/environment-variable-expansion-in-working-directory/11322
+# See: https://discuss.circleci.com/t/circle-2-0-global-environment-variables/8681
+#=====================================================================================================================
+(
+  echo 'export PATH=$PATH:$HOME/bin'
+  echo 'export TERMINUS_HIDE_UPDATE_MESSAGE=1'
+) >> $BASH_ENV
+source $BASH_ENV
+
 set -ex
 
 cd ~/terminus_build_tools_plugin
