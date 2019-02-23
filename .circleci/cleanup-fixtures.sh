e9511e3555a9f2a64e1dc8c22ef5982c73840a46
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-02-23 13:44:45 -0800

    Straighten up fixture cleanup

--- .circleci/cleanup-fixtures.sh
+++ .circleci/cleanup-fixtures.sh
@@ -5,8 +5,15 @@ set -e
 TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
 
 # Delete our github repository and Pantheon site
-# TODO: Maybe allow user to set an environment variable to control this?
-# terminus build:env:obliterate -n --yes "$TERMINUS_SITE"
+#
+# Set an environment variable OBLITERATE to control whether to delete our fixtures
+#   - always: Always delete fixtures (default)
+#   - ok:     Only obliterate if the tests pass; otherwise retain
+#   - fail:   Only obliterate if the tests fail.
+#   - never:  Never delete fixtures (or any other value other than empty)
+if [ -z "$OBLITERATE" ] || [ "x$OBLITERATE" == "xalways" ] || [ "x$OBLITERATE" == "x$1" ] ; then
+  terminus build:env:obliterate -n --yes "$TERMINUS_SITE"
+fi
 
-# Delete any ssh key we may have created for this test
+# Delete old ssh keys; allow the most-recently-created 12 to remain
 for key in $(terminus ssh-key:list --format=csv --fields=ID,Description 2>/dev/null | grep ci-bot-build-tools | sed -e 's/ci-bot-build-tools-//' | sort -rg | sed -e '1,12d' | sed -e 's/,.*//') ; do echo "Remove $key"; terminus ssh-key:remove $key ; done
