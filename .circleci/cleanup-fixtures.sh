c25acc6c5309455574a232567edd8351e4d27b13
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-02-23 10:56:01 -0800

    Fix key removal script. Temporarily disable build environment obiteration after test completion.

--- .circleci/cleanup-fixtures.sh
+++ .circleci/cleanup-fixtures.sh
@@ -5,7 +5,8 @@ set -e
 TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
 
 # Delete our github repository and Pantheon site
-terminus build:env:obliterate -n --yes "$TERMINUS_SITE"
+# TODO: Maybe allow user to set an environment variable to control this?
+# terminus build:env:obliterate -n --yes "$TERMINUS_SITE"
 
 # Delete any ssh key we may have created for this test
-for key in $(terminus ssh-key:list --fields=Description,ID 2>/dev/null | grep "ci-bot-build-tools-$CIRCLE_BUILD_NUM" | sed -e 's/ *[^ ]* *//') ; do echo "Remove $key"; terminus ssh-key:remove $key ; done
+for key in $(terminus ssh-key:list --format=csv --fields=ID,Description 2>/dev/null | grep ci-bot-build-tools | sed -e 's/ci-bot-build-tools-//' | sort -rg | sed -e '1,12d' | sed -e 's/,.*//') ; do echo "Remove $key"; terminus ssh-key:remove $key ; done
