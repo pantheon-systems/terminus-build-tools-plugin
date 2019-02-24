a9f58a49bc2ee514edb7d8d444bd89af91b12d83
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-02-24 14:10:15 -0800

    Simplify scripts

---
+++ .circleci/test-github-repo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+BUILD_TOOLS_VERSION="dev-master"
+if [[ -n "$CIRCLE_BRANCH" ]]; then
+    BUILD_TOOLS_VERSION="dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
+fi
+
+TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
+SOURCE_COMPOSER_PROJECT="$1"
+TARGET_REPO=$GITHUB_USER/$TERMINUS_SITE
+CLONE_URL="https://github.com/${TARGET_REPO}.git"
+
+# Build a test project on github
+terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --git=github --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
+# Confirm that the Pantheon site was created
+terminus site:info "$TERMINUS_SITE"
+# Confirm that the Github or Bitbucket project was created
+git clone "$CLONE_URL" "$TARGET_REPO_WORKING_COPY"
