14002b7852a275fa1b061fd3291f33243c5ee643
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-02-24 12:43:04 -0800

    Try putting environment variables in config.yml

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-#=====================================================================================================================
-# EXPORT needed environment variables
-#
-# Circle CI 2.0 does not yet expand environment variables so they have to be manually EXPORTed
-# Once environment variables can be expanded this section can be removed
-# See: https://discuss.circleci.com/t/unclear-how-to-work-with-user-variables-circleci-provided-env-variables/12810/11
-# See: https://discuss.circleci.com/t/environment-variable-expansion-in-working-directory/11322
-# See: https://discuss.circleci.com/t/circle-2-0-global-environment-variables/8681
-#=====================================================================================================================
-(
-  echo 'export PATH=$PATH:$HOME/bin'
-  echo 'export TERMINUS_HIDE_UPDATE_MESSAGE=1'
-  echo 'export TARGET_REPO_WORKING_COPY=$HOME/system-under-test'
-) >> $BASH_ENV
-source $BASH_ENV
-
 set -ex
 
 cd ~/terminus_build_tools_plugin
