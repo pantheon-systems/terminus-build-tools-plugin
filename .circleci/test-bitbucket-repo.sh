9794088398b8f799ef8bc5c1e92a1250ee052558
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-02-24 13:56:07 -0800

    Further simplify the scripts

--- .circleci/test-bitbucket-repo.sh
+++ .circleci/test-bitbucket-repo.sh
@@ -2,47 +2,17 @@
 
 set -e
 
+BUILD_TOOLS_VERSION="dev-master"
+if [[ -n "$CIRCLE_BRANCH" ]]; then
+    BUILD_TOOLS_VERSION="dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
+fi
+
 TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
-
-# If we are on the master branch
-if [[ $CIRCLE_BRANCH == "master" ]]
-then
-    PR_BRANCH="dev-master"
-else
-    # If this is a pull request use the PR number
-    if [[ -z "$CIRCLE_PULL_REQUEST" ]]
-    then
-        # Stash PR number
-        PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
-
-        # Multidev name is the pull request number
-        PR_BRANCH="pr-$PR_NUMBER"
-    else
-        # Otherwise use the build number
-        PR_BRANCH="dev-$CIRCLE_BUILD_NUM"
-    fi
-fi
-
 SOURCE_COMPOSER_PROJECT="$1"
-
-# If we are on the 1.x branch set the build tools version to 1.x
-if [[ $CIRCLE_BRANCH == "1.x" ]]
-then
-    BUILD_TOOLS_VERSION="${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
-# Otherwise use the current branch
-else
-    # If on root repo use the current branch
-    if [[ $CIRCLE_PROJECT_USERNAME == "pantheon-systems" ]]; then
-        BUILD_TOOLS_VERSION="dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
-    # Otherwise use the dev tip from the pantheon-systems repo
-    else
-        BUILD_TOOLS_VERSION="dev-master"
-    fi
-fi
-
 TARGET_REPO=$BITBUCKET_USER/$TERMINUS_SITE
 CLONE_URL="https://$BITBUCKET_USER@bitbucket.org/${TARGET_REPO}.git"
 
+# Build a test project on bitbucket
 terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --git=bitbucket --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
 # Confirm that the Pantheon site was created
 terminus site:info "$TERMINUS_SITE"
