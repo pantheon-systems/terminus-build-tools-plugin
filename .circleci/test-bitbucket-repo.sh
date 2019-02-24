90d4d686d0b572d6191f0d6b620410870a6f7d1d
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-02-24 12:27:02 -0800

    Denormalize test script to reduce conditionals

--- .circleci/test-bitbucket-repo.sh
+++ .circleci/test-bitbucket-repo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# This was `set -ex`, but removed echo to avoid leaking $BITBUCKET_PASS
-# TODO: We should also pass the $GITHUB_TOKEN when cloning the GitHub repo so that it can be a private repo if desired.
 set -e
 
 TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
@@ -26,9 +24,6 @@ else
 fi
 
 SOURCE_COMPOSER_PROJECT="$1"
-TARGET_REPO_WORKING_COPY=$HOME/$TERMINUS_SITE
-GIT_PROVIDER="$2"
-
 
 # If we are on the 1.x branch set the build tools version to 1.x
 if [[ $CIRCLE_BRANCH == "1.x" ]]
@@ -45,27 +40,11 @@ else
     fi
 fi
 
-if [ "$GIT_PROVIDER" == "github" ]; then
-    TARGET_REPO=$GITHUB_USER/$TERMINUS_SITE
-    CLONE_URL="https://github.com/${TARGET_REPO}.git"
-else
-    if [ "$GIT_PROVIDER" == "bitbucket" ]; then
-        TARGET_REPO=$BITBUCKET_USER/$TERMINUS_SITE
-        CLONE_URL="https://$BITBUCKET_USER@bitbucket.org/${TARGET_REPO}.git"
-    else
-        echo "Unsupported GIT_PROVIDER. Valid values are: github, bitbucket"
-        exit 1
-    fi
-fi
+TARGET_REPO=$BITBUCKET_USER/$TERMINUS_SITE
+CLONE_URL="https://$BITBUCKET_USER@bitbucket.org/${TARGET_REPO}.git"
 
-terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --git=$GIT_PROVIDER --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
+terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --git=bitbucket --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
 # Confirm that the Pantheon site was created
 terminus site:info "$TERMINUS_SITE"
 # Confirm that the Github or Bitbucket project was created
 git clone "$CLONE_URL" "$TARGET_REPO_WORKING_COPY"
-# Confirm that Circle was configured for testing, and that the first test passed.
-
-(
-    set +ex
-    cd "$TARGET_REPO_WORKING_COPY" && circle token "$CIRCLE_TOKEN" && circle watch
-)
---
+++ .circleci/test-github-repo.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This was `set -ex`, but removed echo to avoid leaking $BITBUCKET_PASS
+# TODO: We should also pass the $GITHUB_TOKEN when cloning the GitHub repo so that it can be a private repo if desired.
+set -e
+
+TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
+
+# If we are on the master branch
+if [[ $CIRCLE_BRANCH == "master" ]]
+then
+    PR_BRANCH="dev-master"
+else
+    # If this is a pull request use the PR number
+    if [[ -z "$CIRCLE_PULL_REQUEST" ]]
+    then
+        # Stash PR number
+        PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
+
+        # Multidev name is the pull request number
+        PR_BRANCH="pr-$PR_NUMBER"
+    else
+        # Otherwise use the build number
+        PR_BRANCH="dev-$CIRCLE_BUILD_NUM"
+    fi
+fi
+
+SOURCE_COMPOSER_PROJECT="$1"
+
+# If we are on the 1.x branch set the build tools version to 1.x
+if [[ $CIRCLE_BRANCH == "1.x" ]]
+then
+    BUILD_TOOLS_VERSION="${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
+# Otherwise use the current branch
+else
+    # If on root repo use the current branch
+    if [[ $CIRCLE_PROJECT_USERNAME == "pantheon-systems" ]]; then
+        BUILD_TOOLS_VERSION="dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
+    # Otherwise use the dev tip from the pantheon-systems repo
+    else
+        BUILD_TOOLS_VERSION="dev-master"
+    fi
+fi
+
+TARGET_REPO=$GITHUB_USER/$TERMINUS_SITE
+CLONE_URL="https://github.com/${TARGET_REPO}.git"
+
+terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --git=github --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION"
+# Confirm that the Pantheon site was created
+terminus site:info "$TERMINUS_SITE"
+# Confirm that the Github or Bitbucket project was created
+git clone "$CLONE_URL" "$TARGET_REPO_WORKING_COPY"
+
+# Confirm that Circle was configured for testing, and that the first test passed.
+(
+    set +ex
+    cd "$TARGET_REPO_WORKING_COPY" && circle token "$CIRCLE_TOKEN" && circle watch
+)
