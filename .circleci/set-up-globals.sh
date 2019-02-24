b6da3d0cd08c82101e64b4a86fc8283704245a37
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-02-24 13:23:16 -0800

    Try removing definition of BASH_ENV

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "Home is $HOME"
-mkdir $HOME
+echo "bash env is $BASH_ENV"
 
 #=====================================================================================================================
 # EXPORT needed environment variables
