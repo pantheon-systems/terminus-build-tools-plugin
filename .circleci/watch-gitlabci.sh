aa7455b785e32d5560f7483279faf9ff1982a833
Brian Thompson <bthompson@mindgrub.com>
2019-02-24 20:23:29 -0500

    Add documentation note for GitLab CI test.

--- .circleci/watch-gitlabci.sh
+++ .circleci/watch-gitlabci.sh
@@ -1,1 +1,2 @@
 # TODO: Watch the GitLab CI build. Exit with an error if that job fails.
+# https://github.com/zaquestion/lab/issues/261 contains the start of functionality to watch tests until they pass.
