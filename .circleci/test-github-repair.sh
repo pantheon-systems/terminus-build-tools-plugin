6879f4fed5bdb211ba6ffbc5e14aabb8561cd1bc
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-03-27 15:43:47 -0700

    Also test build:project:repair

---
+++ .circleci/test-github-repair.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+SOURCE_COMPOSER_PROJECT="$1"
+
+# Repair the test project on github
+terminus build:project:repair -n "$TERMINUS_SITE" --email="$GIT_EMAIL"
+
+# Make a branch so that we can test a PR
+cd "$TARGET_REPO_WORKING_COPY"
+git checkout -b test-after-repair
+echo 'Test after repair' >> README.md
+git add README.md
+git commit -m "Test after repair"
+git push origin test-after-repair
+hub pull-request -m "Test after repair" -b master -h test-after-repair
+
+
