56d15e2dafc530fa8e3f72a615e8ea81d712b71d
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-03-27 16:57:52 -0700

    Authenticate with the GITHUB_TOKEN when pushing branch to GitHub to test a PR

--- .circleci/test-github-repair.sh
+++ .circleci/test-github-repair.sh
@@ -13,7 +13,12 @@ git checkout -b test-after-repair
 echo 'Test after repair' >> README.md
 git add README.md
 git commit -m "Test after repair"
-git push origin test-after-repair
+
+# Push the branch
+ORIGIN="https://$GITHUB_TOKEN:x-oauth-basic@github.com/$GITHUB_USER/$TERMINUS_SITE.git"
+git push $ORIGIN test-after-repair | sed -e "s/$GITHUB_TOKEN/[REDACTED]/g"
+
+# Create the pull request
 hub pull-request -m "Test after repair" -b master -h test-after-repair
 
 
