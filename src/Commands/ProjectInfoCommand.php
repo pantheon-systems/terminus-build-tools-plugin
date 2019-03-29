7312da53e1360ac5c9028a146ef42895cd889ea0
Greg Anderson <greg.1.anderson@greenknowe.org>
2019-03-29 13:23:16 -0700

    Fix typo in GitLab badge graphic url

---
+++ src/Commands/ProjectInfoCommand.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Terminus Plugin that contain a collection of commands useful during
+ * the build step on a [Pantheon](https://www.pantheon.io) site that uses
+ * a GitHub PR workflow.
+ *
+ * See README.md for usage information.
+ */
+
+namespace Pantheon\TerminusBuildTools\Commands;
+
+use Consolidation\OutputFormatters\StructuredData\PropertyList;
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Pantheon\Terminus\Commands\TerminusCommand;
+use Pantheon\Terminus\Exceptions\TerminusException;
+use Pantheon\Terminus\Site\SiteAwareInterface;
+use Pantheon\Terminus\Site\SiteAwareTrait;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Process\ProcessUtils;
+use Consolidation\AnnotatedCommand\AnnotationData;
+use Consolidation\AnnotatedCommand\CommandData;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Composer\Semver\Comparator;
+
+/**
+ * Project Repair Command
+ */
+class ProjectInfoCommand extends BuildToolsBase
+{
+    use \Pantheon\TerminusBuildTools\Task\Ssh\Tasks;
+    use \Pantheon\TerminusBuildTools\Task\CI\Tasks;
+
+    /**
+     * Show build info attached to a site created by the
+     * build:project:create command.
+     *
+     * @command build:project:info
+     */
+    public function info(
+        $site_name,
+        $options = [
+            'ci' => '',
+        ])
+    {
+        // Fetch the build metadata
+        $buildMetadata = $this->retrieveBuildMetadata("{$site_name}.dev");
+        $url = $this->getMetadataUrl($buildMetadata);
+
+        $this->log()->notice('Build metadata: {metadata}', ['metadata' => var_export($buildMetadata, true)]);
+
+        // Create a git repository service provider appropriate to the URL
+        $this->git_provider = $this->inferGitProviderFromUrl($url);
+
+        // Extract just the project id from the URL
+        $target_project = $this->projectFromRemoteUrl($url);
+        $this->git_provider->getEnvironment()->setProjectId($target_project);
+
+        $ci_provider_class_or_alias = $this->selectCIProvider($this->git_provider->getServiceName(), $options['ci']);
+
+        $this->ci_provider = $this->createCIProvider($ci_provider_class_or_alias);
+
+        // Ensure that all of our providers are given the credentials they need.
+        // Providers are not usable until this is done.
+        $this->providerManager()->validateCredentials();
+
+        // Fetch the project name.
+        $this->log()->notice('Found project {project}', ['project' => $target_project]);
+
+    }
+}
