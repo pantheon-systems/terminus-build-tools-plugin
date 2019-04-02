890604cca33100e2c4b1621018b7823f90e0363c
Brian Thompson <bthompson@mindgrub.com>
2019-04-02 17:51:52 -0400

    Add test.

---
+++ .circleci/test-secrets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
+
+terminus build:secrets:set $TERMINUS_SITE key val --file=build-testing.json
+terminus build:secrets:set $TERMINUS_SITE author rvtraveller --file=build-testing.json
+
+# Confirm that we can retrieve a value
+singlevalue=$(terminus build:secrets:show $TERMINUS_SITE author --file=build-testing.json)
+
+test singlevalue = "rvtraveller"
+
+terminus build:secrets:delete $TERMINUS_SITE key --file=build-testing.json
+terminus build:secrets:delete $TERMINUS_SITE author --file=build-testing.json
+
+emptyvalues=$(terminus biold:secrets:list $TERMINUS_SITE --file=build-testing.json)
+
+test emptyvalues = ""
+
