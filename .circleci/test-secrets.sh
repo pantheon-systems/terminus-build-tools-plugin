a7a547f79129aa47b9c4dd478c2a6f4ca5473a47
Brian Thompson <bthompson@mindgrub.com>
2019-04-02 21:14:14 -0400

    Fix test for secrets.

--- .circleci/test-secrets.sh
+++ .circleci/test-secrets.sh
@@ -3,19 +3,20 @@
 set -e
 
 TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
+TERMINUS_ENV=dev
 
-terminus build:secrets:set $TERMINUS_SITE key val --file=build-testing.json
-terminus build:secrets:set $TERMINUS_SITE author rvtraveller --file=build-testing.json
+terminus build:secrets:set "$TERMINUS_SITE.$TERMINUS_ENV" key val --file=build-testing.json
+terminus build:secrets:set "$TERMINUS_SITE.$TERMINUS_ENV" author rvtraveller --file=build-testing.json
 
 # Confirm that we can retrieve a value
-singlevalue=$(terminus build:secrets:show $TERMINUS_SITE author --file=build-testing.json)
+singlevalue=$(terminus build:secrets:show "$TERMINUS_SITE.$TERMINUS_ENV" author --file=build-testing.json)
 
 test singlevalue = "rvtraveller"
 
-terminus build:secrets:delete $TERMINUS_SITE key --file=build-testing.json
-terminus build:secrets:delete $TERMINUS_SITE author --file=build-testing.json
+terminus build:secrets:delete "$TERMINUS_SITE.$TERMINUS_ENV" key --file=build-testing.json
+terminus build:secrets:delete "$TERMINUS_SITE.$TERMINUS_ENV" author --file=build-testing.json
 
-emptyvalues=$(terminus build:secrets:list $TERMINUS_SITE --file=build-testing.json)
+emptyvalues=$(terminus build:secrets:list "$TERMINUS_SITE.$TERMINUS_ENV" --file=build-testing.json)
 
 test emptyvalues = ""
 
