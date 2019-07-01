bd97ad447db8e6b6e7adaea984ae2e26b19a3c56
Aaron Bauman <aaron@messageagency.com>
2019-07-01 16:33:12 -0400

    Add OAuth support for BitBucket API

--- src/API/Bitbucket/BitbucketAPI.php
+++ src/API/Bitbucket/BitbucketAPI.php
@@ -2,6 +2,7 @@
 
 namespace Pantheon\TerminusBuildTools\API\Bitbucket;
 
+use Pantheon\Terminus\Exceptions\TerminusException;
 use Pantheon\TerminusBuildTools\API\WebAPI;
 use Pantheon\TerminusBuildTools\API\WebAPIInterface;
 use Pantheon\TerminusBuildTools\ServiceProviders\ProviderEnvironment;
@@ -18,6 +19,8 @@ class BitbucketAPI extends WebAPI
     const BITBUCKET_USER = 'BITBUCKET_USER';
     const BITBUCKET_PASS = 'BITBUCKET_PASS';
     const BITBUCKET_AUTH = 'BITBUCKET_AUTH';
+    const BITBUCKET_TOKEN = 'BITBUCKET_TOKEN';
+    const BITBUCKET_TOKEN_EXPIRATION = 'BITBUCKET_TOKEN_EXPIRATION';
 
     public function serviceHumanReadableName()
     {
@@ -35,13 +38,54 @@ class BitbucketAPI extends WebAPI
             'User-Agent' => ProviderEnvironment::USER_AGENT,
         ];
 
-        return new \GuzzleHttp\Client(
+        // If we have an auth token, instantiate OAuth client.
+        if ($auth_token = $this->getAuthToken()) {
+            $headers['Authorization'] = 'Bearer ' . $auth_token;
+            return new \GuzzleHttp\Client(
+                [
+                    'base_uri' => 'https://api.bitbucket.org/2.0/',
+                    'headers' => $headers,
+                ]
+            );
+        }
+
+      // Otherwise, instantiate the basic auth client.
+      return new \GuzzleHttp\Client(
             [
                 'base_uri' => 'https://api.bitbucket.org/2.0/',
                 'auth' => [ $this->serviceTokenStorage->token(self::BITBUCKET_USER), $this->serviceTokenStorage->token(self::BITBUCKET_PASS) ],
                 'headers' => $headers,
             ]
         );
+    }
+
+    protected function getAuthToken() {
+        if (!$this->serviceTokenStorage->hasToken(self::BITBUCKET_USER)
+        || !$this->serviceTokenStorage->hasToken(self::BITBUCKET_PASS)) {
+            return FALSE;
+        }
+
+        if ($this->serviceTokenStorage->hasToken(self::BITBUCKET_TOKEN)
+        && $this->serviceTokenStorage->hasToken(self::BITBUCKET_TOKEN_EXPIRATION)
+        && $this->serviceTokenStorage->hasToken(self::BITBUCKET_TOKEN_EXPIRATION) > time()) {
+            return $this->serviceTokenStorage->token(self::BITBUCKET_TOKEN);
+        }
+
+        // Post a request for new auth token.
+        $client = new \GuzzleHttp\Client();
+        $authResponse = $client->request('POST', 'https://bitbucket.org/site/oauth2/access_token',
+            [
+                'auth' => [$this->serviceTokenStorage->token(self::BITBUCKET_USER),  $this->serviceTokenStorage->token(self::BITBUCKET_PASS)],
+              'form_params' => ['grant_type' => 'client_credentials']
+            ]
+        );
+        if ($authResponse->getStatusCode() == '200') {
+            $response = json_decode($authResponse->getBody()->getContents());
+            $this->serviceTokenStorage->setToken(self::BITBUCKET_TOKEN, $response->access_token);
+            $this->serviceTokenStorage->setToken(self::BITBUCKET_TOKEN_EXPIRATION, time() + (int)$response->expires_in);
+            return $response->access_token;
+        }
+        return FALSE;
     }
 
     protected function isPagedResponse($headers)
