55e9908362d36b759511c56ddf2a200c872bb781
Brian Thompson <bthompson@mindgrub.com>
2019-08-13 18:51:02 -0400

    Adjust setup script to retrieve token.

--- .circleci/configure-selfhosted-gitlab.sh
+++ .circleci/configure-selfhosted-gitlab.sh
@@ -5,8 +5,12 @@
 #
 #=====================================================================================================================
 
+# For safety, just sleep for 30 seconds to try and make sure GitLab is good.
+sleep 30
+
 SIGNIN_URL=$(curl -Ls -o /dev/null -w %{url_effective} -k https://gitlab-secure/)
 
 RESET_TOKEN=$(sed "s/https:\/\/gitlab\-secure\/users\/password\/edit?reset_password_token=//g" <<< $SIGNIN_URL)
 
+echo "Reset token:"
 echo $RESET_TOKEN
