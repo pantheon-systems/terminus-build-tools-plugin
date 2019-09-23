5949eeea3e0adb6cfc5fe026a611522d8d9bf2ac
Daniel Bachhuber <daniel@handbuilt.co>
2019-09-23 11:08:25 -0700

    Add a new `build:comment:add:pr` command for creating comments on PRs

---
+++ src/Commands/CommentAddPullRequestCommand.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Terminus Plugin that contain a collection of commands useful during
+ * the build step on a [Pantheon](https://www.pantheon.io) site that uses
+ * a GitHub PR workflow.
+ *
+ * See README.md for usage information.
+ */
+
+namespace Pantheon\TerminusBuildTools\Commands;
+
+use Pantheon\Terminus\Exceptions\TerminusException;
+
+/**
+ * Pull Request Comment Command
+ */
+class CommentAddPullRequestCommand extends BuildToolsBase
+{
+
+    /**
+     * Add a comment to a specified PR on the repository.
+     *
+     * @authorize
+     *
+     * @command build:comment:add:pr
+     */
+    public function commentAddPullRequest(
+        $options = [
+            'pr_id' => '',
+            'message' => '',
+            'site_url' => ''
+        ])
+    {
+        // Get current repository and commit
+        $remoteUrlFromGit = exec('git config --get remote.origin.url');
+        $prId = $options['pr_id'];
+        if (empty($prId)) {
+            throw new TerminusException( '--pr_id=<id> is required.' );
+        }
+
+        // Create a Git repository service provider appropriate to the URL
+        $this->inferGitProviderFromUrl($remoteUrlFromGit);
+
+        // Ensure that credentials for the Git provider are available
+        $this->providerManager()->validateCredentials();
+
+        // Compile message
+        if (!empty($options['site_url'])) {
+            $message = "[![Visit Site](https://raw.githubusercontent.com/pantheon-systems/ci-drops-8/0.1.0/data/img/visit-site-36.png)](".$options['site_url'].")\n\n".$options['message'];
+        } else {
+            $message = $options['message'];
+        }
+
+        if (!empty($message)) {
+            // Submit message
+            $targetProject = $this->projectFromRemoteUrl($remoteUrlFromGit);
+            $this->git_provider->commentOnPullRequest($targetProject, $prId, $message);
+        }
+    }
+}
