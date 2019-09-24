869860561a5ff9283ba4f5962a1e72bb596c5164
Nick Wilde <nwilde@taoti.com>
2019-09-24 13:42:58 -0400

    message/error checking improvements

--- src/Commands/BuildToolsBase.php
+++ src/Commands/BuildToolsBase.php
@@ -812,15 +812,19 @@ class BuildToolsBase extends TerminusCommand implements SiteAwareInterface, Buil
         // Create a new branch and commit the results from anything that may
         // have changed. We presume that the source repository is clean of
         // any unwanted files prior to the build step (e.g. after a clean
-        // checkout in a CI environment.)
+        // checkout in a CI environment). If the --prepare-for-pantheon flag
+        // is passed, run composer prepare-for-pantheon and don't force add.
         $this->passthru("git -C $repositoryDir checkout -B $branch");
         if ($prepareForPantheon) {
-          $this->log()->notice('Determine whether prepare-for-pantheon exists for {site}', ['site' => $site]);
-          exec("composer help build-assets", $outputLines, $status);
+          exec("composer help prepare-for-pantheon", $outputLines, $status);
           if (!$status) {
             $this->passthru('composer prepare-for-pantheon');
+            $this->passthru("git -C $repositoryDir add -A");
           }
-          $this->passthru("git -C $repositoryDir add -A");
+          else {
+            $this->log()->notice('composer prepare-for-pantheon failed for {site}. Falling back on git add --force', ['site' => $site]);
+            $this->passthru("git -C $repositoryDir add --force -A .");
+          }
         }
         else {
           $this->passthru("git -C $repositoryDir add --force -A .");
