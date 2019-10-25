3e80bb4b69172d6a426774896904d41741623bc6
Andrew Taylor <andrew@ataylor.me>
2019-10-25 14:48:41 -0700

    Move TERMINUS_SITE declaration to set up globals

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -12,6 +12,7 @@
 (
   echo 'export PATH=$PATH:$HOME/bin'
   echo 'export TARGET_REPO_WORKING_COPY=$HOME/system-under-test'
+  echo 'export TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM'
 ) >> $BASH_ENV
 source $BASH_ENV
 
