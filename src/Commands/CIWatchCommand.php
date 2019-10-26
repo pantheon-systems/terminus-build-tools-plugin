a396e65bf3e7dec2ea7af3924dc2708c6b63505c
Andrew Taylor <andrew@ataylor.me>
2019-10-26 09:53:47 -0700

    Setup providers and validate credentials in hooks

--- src/Commands/CIWatchCommand.php
+++ src/Commands/CIWatchCommand.php
@@ -3,6 +3,7 @@
 namespace Pantheon\TerminusBuildTools\Commands;
 
 use Pantheon\Terminus\Exceptions\TerminusException;
+use Symfony\Component\Console\Input\InputInterface;
 
 /**
  * CI Watch Command
@@ -10,10 +11,64 @@ use Pantheon\Terminus\Exceptions\TerminusException;
 class CIWatchCommand extends BuildToolsBase
 {
 
+    use \Pantheon\TerminusBuildTools\Task\CI\Tasks;
+
     /**
      * @var string
      */
     protected $target_project;
+
+    /**
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * Initialize the default value for selected options.
+     *
+     * @hook init build:ci:watch
+     */
+    public function initOptionValues(InputInterface $input)
+    {
+        $this->log()->notice('build:ci:watch @hook init');
+
+        // Get the site name argument. If there is none, skip init
+        // and allow the command to fail with "not enough arguments"
+        $site_name = $input->getArgument('site_name');
+        if (empty($site_name)) {
+            return;
+        }
+
+        // Fetch the build metadata
+        $buildMetadata = $this->retrieveBuildMetadata("{$site_name}.dev");
+        $url = $this->getMetadataUrl($buildMetadata);
+        $this->url = $url;
+
+        // Create a git repository service provider appropriate to the URL
+        $this->git_provider = $this->inferGitProviderFromUrl($url);
+
+        // Extract just the project id from the URL
+        $target_project = $this->projectFromRemoteUrl($url);
+        $this->target_project = $target_project;
+        $this->git_provider->getEnvironment()->setProjectId($target_project);
+
+        $ci_provider_class_or_alias = $this->selectCIProvider($this->git_provider->getServiceName(), '');
+
+        $this->createCIProvider($ci_provider_class_or_alias);
+    }
+
+    /**
+     * Ensure that the user has not supplied any parameters with invalid values.
+     *
+     * @hook validate build:ci:watch
+     */
+    public function validateCreateProject()
+    {
+        $this->log()->notice('build:ci:watch @hook validate');
+
+        // Ensure that all of our providers are given the credentials they requested.
+        $this->providerManager()->validateCredentials();
+    }
 
     /**
      * Watches the most recent job for a CI provider to see how it finishes.
@@ -31,24 +86,16 @@ class CIWatchCommand extends BuildToolsBase
         ]
     )
     {
-
-        $buildMetadata = $this->retrieveBuildMetadata("$site_name.dev");
-        $url = $this->getMetadataUrl($buildMetadata);
-        $this->target_project = $this->projectFromRemoteUrl($url);
-        $this->inferGitProviderFromUrl($url);
-
-        // Ensure that credentials for the Git provider are available.
-        $this->providerManager()->validateCredentials();
-        $ciProvider = $this->providerManager()->inferProvider($url, 'Pantheon\TerminusBuildTools\ServiceProviders\CIProviders\CIProvider');
         $ci_env = $this->getCIEnvironment([]);
-        $repository = $this->git_provider->getEnvironment();
-        $repository->setProjectId($this->target_project);
-        $ci_env->storeState('repository', $repository);
+        // Add the environment variables from the git provider to the CI environment.
+        $ci_env->storeState('repository', $this->git_provider->getEnvironment());
 
         // TODO Fetch the default branch from Git provider.
         if (empty($options['branch-name'])) {
             $options['branch-name'] = 'master';
         }
+
+        $ciProvider = $this->ci_provider;
 
         // Fetch all pipelines (associated with a specific branch) from provider.
         $pipelineId = $ciProvider->getMostRecentPipelineId($ci_env, $options['branch-name']);
