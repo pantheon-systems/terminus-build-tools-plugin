06bf32885af33845bcf35d617626b8dae9479ad1
Andrew Taylor <andrew@ataylor.me>
2019-10-27 10:10:03 -0700

    Test GitHub D8 with new test scripts

--- .circleci/test-github-repo.sh
+++ .circleci/test-github-repo.sh
@@ -5,21 +5,20 @@ set -e
 SOURCE_COMPOSER_PROJECT="$1"
 EXTRA_ARGS="$2"
 
-BUILD_TOOLS_VERSION="dev-master"
-if [[ -n "$CIRCLE_BRANCH" ]]; then
-    BUILD_TOOLS_VERSION="dev-${CIRCLE_BRANCH}#${CIRCLE_SHA1}"
-fi
+#BUILD_TOOLS_VERSION="dev-master"
+#if [[ -n "$CIRCLE_BRANCH" ]]; then
+#    BUILD_TOOLS_VERSION="dev-${CIRCLE_BRANCH}"
+#fi
+unset BUILD_TOOLS_VERSION
 
-TERMINUS_SITE=build-tools-$CIRCLE_BUILD_NUM
-TARGET_REPO=$GITHUB_USER/$TERMINUS_SITE
-CLONE_URL="https://github.com/${TARGET_REPO}.git"
+CLONE_URL="git@github.com:$GITLAB_USER/$TERMINUS_SITE.git"
 
 # Clear Composer cache before running build:project:create
 composer clear-cache
 
-# Build a test project on github
-terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION" $EXTRA_ARGS
+# Build a test project on gitlab
+terminus build:project:create -n "$SOURCE_COMPOSER_PROJECT" "$TERMINUS_SITE" --git=gitlab --team="$TERMINUS_ORG" --email="$GIT_EMAIL" --env="BUILD_TOOLS_VERSION=$BUILD_TOOLS_VERSION" $EXTRA_ARGS
 # Confirm that the Pantheon site was created
 terminus site:info "$TERMINUS_SITE"
-# Confirm that the Github project was created
+# Confirm that the GitLab project was created
 git clone "$CLONE_URL" "$TARGET_REPO_WORKING_COPY"
