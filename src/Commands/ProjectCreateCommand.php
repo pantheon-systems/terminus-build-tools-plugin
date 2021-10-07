8c3f3fbc70f960d0b902e08c1cb84ab7a3691d18
Kevin Porras <kporras07@gmail.com>
2021-10-07 16:14:50 -0600

    Rename tbt-ci-integrations repo. (#407)

--- src/Commands/ProjectCreateCommand.php
+++ src/Commands/ProjectCreateCommand.php
@@ -262,7 +262,7 @@ class ProjectCreateCommand extends BuildToolsBase
             'visibility' => 'public',
             'region' => '',
             'template-repository' => '',
-            'ci-template' => 'git@github.com:pantheon-systems/tbt-ci-integrations.git',
+            'ci-template' => 'git@github.com:pantheon-systems/tbt-ci-templates.git',
             'clu-cron-pattern' => '',
         ])
     {
