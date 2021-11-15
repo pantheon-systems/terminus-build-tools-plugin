34ea38540fa87e8cd8abae7fe7cc2e4c309c5ac7
Kevin Porras <kporras07@gmail.com>
2021-11-15 14:50:28 -0600

    Change d9 alias to use drupal-recommended upstream.

--- src/Commands/BuildToolsBase.php
+++ src/Commands/BuildToolsBase.php
@@ -321,7 +321,7 @@ class BuildToolsBase extends TerminusCommand implements SiteAwareInterface, Buil
         // pantheon-systems is assumed.
         //
         $aliases = [
-            'git@github.com:pantheon-upstreams/drupal-project.git' => ['d9', 'drops-9'],
+            'git@github.com:pantheon-upstreams/drupal-recommended.git' => ['d9', 'drops-9'],
             'example-drops-8-composer' => ['d8', 'drops-8'],
             'example-drops-7-composer' => ['d7', 'drops-7'],
             'example-wordpress-composer' => ['wp', 'wordpress'],
