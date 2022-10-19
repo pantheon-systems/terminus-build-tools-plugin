fd72ea3fb21ed108654e6225c526f5b6a07214a0
Kevin Porras <kevin.porras@pantheon.io>
2022-10-19 10:59:26 -0600

    Also add pushback to wordpress apps.

--- src/Commands/ProjectCreateCommand.php
+++ src/Commands/ProjectCreateCommand.php
@@ -374,12 +374,12 @@ class ProjectCreateCommand extends BuildToolsBase
             // Require basic testing general packages.
             exec("composer --working-dir=$siteDir require --no-update --dev dealerdirect/phpcodesniffer-composer-installer squizlabs/php_codesniffer phpunit/phpunit");
             exec("composer --working-dir=$siteDir config allow-plugins.dealerdirect/phpcodesniffer-composer-installer true");
+            exec("composer --working-dir=$siteDir require --no-update pantheon-systems/quicksilver-pushback");
 
             // Install packages depending on the application.
             if ($app === 'Drupal') {
                 exec("composer --working-dir=$siteDir require --no-update --dev drupal/coder drupal/drupal-extension drupal/drupal-driver");
                 exec("composer --working-dir=$siteDir require --no-update drush-ops/behat-drush-endpoint");
-                exec("composer --working-dir=$siteDir require --no-update pantheon-systems/quicksilver-pushback");
                 exec("composer --working-dir=$siteDir require --no-update --dev behat/behat behat/mink dmore/behat-chrome-extension genesis/behat-fail-aid jcalderonzumba/mink-phantomjs-driver mikey179/vfsstream");
             } elseif (strtolower($app) === 'wordpress') {
                 exec("composer --working-dir=$siteDir require --no-update --dev wp-coding-standards/wpcs");
